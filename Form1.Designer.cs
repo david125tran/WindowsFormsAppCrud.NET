ea991205de6cad032766b762538744f1588cde21
David Tran <109535790+david125tran@users.noreply.github.com>
2023-07-23 11:50:18 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WindowsFormsAppCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FirstName = new System.Windows.Forms.Label();
+            this.LastName = new System.Windows.Forms.Label();
+            this.Position = new System.Windows.Forms.Label();
+            this.Salary = new System.Windows.Forms.Label();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.tbPosition = new System.Windows.Forms.TextBox();
+            this.tbSalary = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FirstName
+            // 
+            this.FirstName.AutoSize = true;
+            this.FirstName.Location = new System.Drawing.Point(22, 19);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(72, 16);
+            this.FirstName.TabIndex = 0;
+            this.FirstName.Text = "First Name";
+            // 
+            // LastName
+            // 
+            this.LastName.AutoSize = true;
+            this.LastName.Location = new System.Drawing.Point(22, 63);
+            this.LastName.Name = "LastName";
+            this.LastName.Size = new System.Drawing.Size(72, 16);
+            this.LastName.TabIndex = 1;
+            this.LastName.Text = "Last Name";
+            // 
+            // Position
+            // 
+            this.Position.AutoSize = true;
+            this.Position.Location = new System.Drawing.Point(22, 107);
+            this.Position.Name = "Position";
+            this.Position.Size = new System.Drawing.Size(55, 16);
+            this.Position.TabIndex = 2;
+            this.Position.Text = "Position";
+            // 
+            // Salary
+            // 
+            this.Salary.AutoSize = true;
+            this.Salary.Location = new System.Drawing.Point(22, 151);
+            this.Salary.Name = "Salary";
+            this.Salary.Size = new System.Drawing.Size(64, 16);
+            this.Salary.TabIndex = 3;
+            this.Salary.Text = "Salary ($)";
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Location = new System.Drawing.Point(25, 38);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(217, 22);
+            this.tbFirstName.TabIndex = 4;
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(25, 82);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(217, 22);
+            this.tbLastName.TabIndex = 5;
+            // 
+            // tbPosition
+            // 
+            this.tbPosition.Location = new System.Drawing.Point(25, 126);
+            this.tbPosition.Name = "tbPosition";
+            this.tbPosition.Size = new System.Drawing.Size(217, 22);
+            this.tbPosition.TabIndex = 6;
+            // 
+            // tbSalary
+            // 
+            this.tbSalary.Location = new System.Drawing.Point(25, 170);
+            this.tbSalary.Name = "tbSalary";
+            this.tbSalary.Size = new System.Drawing.Size(217, 22);
+            this.tbSalary.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(22, 198);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(214, 40);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(248, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(699, 413);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(22, 290);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(214, 40);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(22, 244);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(214, 40);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 450);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbSalary);
+            this.Controls.Add(this.tbPosition);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.Salary);
+            this.Controls.Add(this.Position);
+            this.Controls.Add(this.LastName);
+            this.Controls.Add(this.FirstName);
+            this.Name = "Form1";
+            this.Text = "Company";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label FirstName;
+        private System.Windows.Forms.Label LastName;
+        private System.Windows.Forms.Label Position;
+        private System.Windows.Forms.Label Salary;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.TextBox tbPosition;
+        private System.Windows.Forms.TextBox tbSalary;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppCRUD
+{
+    public partial class Form1 : Form
+    {
+        // Create an object model for our table
+        Employee employeeModel = new Employee();
+        public Form1()
+        {
+            InitializeComponent();      // This class loads the GUI when we run
+        }
+
+        //------------------------CREATE------------------------
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Make an object off the database
+            EmployeesDBEntities employeesDB = new EmployeesDBEntities();
+            // Add the textbox entries to the object
+            employeeModel.FirstName = tbFirstName.Text;
+            employeeModel.LastName = tbLastName.Text;   
+            employeeModel.Salary = tbSalary.Text;
+            employeeModel.Position = tbPosition.Text;
+            employeesDB.Employees.Add(employeeModel);
+            employeesDB.SaveChanges();
+            MessageBox.Show("The database has been updated");
+
+            // Refresh the grid
+            populateDataGridView();
+
+        }
+
+        //------------------------READ------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Load all of the data from the database by calling populateDataGridView()
+            populateDataGridView();
+        }
+
+        //------------------------READ------------------------
+        private void populateDataGridView()
+        {
+            // Load all of the data from the SQL database
+            EmployeesDBEntities db = new EmployeesDBEntities();
+            dataGridView1.DataSource = db.Employees.ToList<Employee>();
+        }
+
+        //------------------------UPDATE------------------------
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            {
+                employeeModel.EmployeeId = (int)dataGridView1.CurrentRow.Cells["EmployeeId"].Value;
+                // Make an object off of the database
+                EmployeesDBEntities db = new EmployeesDBEntities();
+                // Push the data to the textboxes in the GUI
+                employeeModel = db.Employees.Where(x => x.EmployeeId == employeeModel.EmployeeId).FirstOrDefault();
+                tbFirstName.Text = employeeModel.FirstName;
+                tbLastName.Text = employeeModel.LastName;
+                tbSalary.Text = employeeModel.Salary;
+                tbPosition.Text = employeeModel.Position;
+
+            }
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // Make an object off the database
+            EmployeesDBEntities employeesDB = new EmployeesDBEntities();
+            // Add the textbox entries to the object
+            employeeModel.FirstName = tbFirstName.Text;
+            employeeModel.LastName = tbLastName.Text;
+            employeeModel.Salary = tbSalary.Text;
+            employeeModel.Position = tbPosition.Text;
+
+            // Add the object to the SQL database
+            if (employeeModel.EmployeeId == 0)                   // Hand                                   
+                employeesDB.Employees.Add(employeeModel);
+            else
+                employeesDB.Entry(employeeModel).State = System.Data.Entity.EntityState.Modified;
+            employeesDB.SaveChanges();
+            MessageBox.Show("The database has been updated");
+
+            // Refresh the grid
+            populateDataGridView();
+        }
+
+        //------------------------DELETE------------------------
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try     // Try to delete a row
+            {
+                EmployeesDBEntities db = new EmployeesDBEntities();
+                var entry = db.Entry(employeeModel);
+                if (entry.State == System.Data.Entity.EntityState.Detached) // Check to see if the entity is detached (the entity is not being tracked by the context)
+                    db.Employees.Attach(employeeModel);                     // Attach the object 
+                db.Employees.Remove(employeeModel);                         // Remove the entry
+                db.SaveChanges();
+                // Refresh the grid
+                populateDataGridView();
+            }
+            catch   // Handle cases where the user did not select a row to delete
+            {
+                MessageBox.Show("You need to select a row to delete");
+                throw;
+            }
+        }
+    }
+}
